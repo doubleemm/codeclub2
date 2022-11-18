7c6bf0cd35c8b4b3869d417d51aa211768f9f7dc
doubleemm <mike.majunke@gmail.com>
2022-11-18 13:20:17 +0100

    working

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const ANSWERS = {
+	1 : "Hello, World!",
+	2 : "Hello, you fool",
+	3 : "Hello, hello, turn your radio on",
+	4 : "Hello, darkness my old friend",
+};
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+
 export default {
 	fetch() {
-		return new Response('Hello worker!', {
+		return new Response(ANSWERS[getRandomInt(1,5)], {
 			headers: {
 				'content-type': 'text/plain',
 			},
